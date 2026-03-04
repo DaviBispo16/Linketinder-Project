9eb1630ece9c60d91cd3a69b4ac695a8549b4725
Davi <davibispo2004@hotmail.com>
2026-03-04 00:33:48 -0300

    feat: added tests for company

--- src/test/groovy/service/CompanyTest.groovy
+++ src/test/groovy/service/CompanyTest.groovy
@@ -1,20 +1,35 @@
-package model
+package service
 
 import com.davi.model.Company
 import com.davi.service.CompanyService
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import static org.junit.jupiter.api.Assertions.*
 
 
 class CompanyTest {
-    private CompanyService companyService;
+    private CompanyService companyService = new CompanyService()
 
     @Test
-    void companyCreatedWithSucess() {
+    @DisplayName("When a company should be sucessfully created")
+    void companyCreatedWithSuccessfully() {
         Company company = new Company('ZG Acelera',   'contato@acelera.com.br',   'GO', '01000-000', 'Software house focada em integração e automação.', '12.345.678/0001-90', 'Brazil')
+        companyService.create(company)
+        assertEquals(1, companyService.listAll().size())
+    }
 
-        List<Company> companies = companyService.create(company)
+    @Test
+    @DisplayName("When a company should be sucessfully listed")
+    void companyListedWithSuccessfully() {
+        Company firstCompany = new Company('FlowTech Solutions',   'contato@flowtech.example',   'SP', '01000-000', 'Software house focada em integração e automação.', '12.345.678/0001-90', 'Brazil')
+        Company secondCompany = new Company('Bahia Payments',       'suporte@bahiapay.example',   'BA', '40000-000', 'Serviços de pagamento e adquirência para varejo.',  '98.765.432/0001-10', 'Brazil')
+        Company thirdCompany = new Company('Rio Data Lab',         'hello@riodatalab.example',   'RJ', '20000-000', 'Consultoria em dados, BI e observabilidade.', '45.678.912/0001-33', 'Brazil')
 
-        assertEquals(1, companies.size())
+        companyService.create(firstCompany)
+        companyService.create(secondCompany)
+        companyService.create(thirdCompany)
+
+        List<Company> companies = companyService.listAll();
+        assertEquals(3, companyService.listAll().size())
     }
 }
