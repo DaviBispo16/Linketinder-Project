bbb1f9be12675215bfd947dcdd73ac31dc85d138
Davi <davibispo2004@hotmail.com>
2026-03-04 20:48:48 -0300

    feat: created service for candidate

--- src/main/groovy/com/davi/service/CandidateService.groovy
+++ src/main/groovy/com/davi/service/CandidateService.groovy
@@ -1,4 +1,16 @@
 package com.davi.service
 
+import com.davi.model.Candidate
+
 class CandidateService {
+    private List<Candidate> candidates = []
+
+    Candidate create(Candidate newCandidate) {
+        candidates << newCandidate
+        return newCandidate
+    }
+
+    List<Candidate> listAll() {
+        return candidates
+    }
 }
