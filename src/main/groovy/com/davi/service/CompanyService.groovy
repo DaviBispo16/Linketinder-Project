8e6afc6829efc1bb9892fe1945dbccca16309cac
Davi <davibispo2004@hotmail.com>
2026-03-04 00:36:27 -0300

    feat: added service for company

--- src/main/groovy/com/davi/service/CompanyService.groovy
+++ src/main/groovy/com/davi/service/CompanyService.groovy
@@ -1,4 +1,17 @@
 package com.davi.service
 
+import com.davi.model.Company
+
 class CompanyService {
+    private final List<Company> companies = []
+
+    Company create(Company company) {
+        companies << company
+        return company;
+    }
+
+    List<Company> listAll() {
+        return companies
+    }
+
 }
