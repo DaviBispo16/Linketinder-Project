9b92e6631744dbba2cd151e81f05589a5c654e7a
Davi <davibispo2004@hotmail.com>
2026-03-24 22:20:03 -0300

    feat(models): added company regex

--- frontend/src/models/Company.ts
+++ frontend/src/models/Company.ts
@@ -7,3 +7,13 @@ export interface Company {
     cep: string;
     description: string;
 }
+
+export const CompanyRegex = {
+    name: /^[A-Za-zÀ-ÖØ-öø-ÿ\s]{2,}$/,
+    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    cnpj: /^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$/,
+    country: /^[A-Za-zÀ-ÖØ-öø-ÿ\s]{2,}$/,
+    state: /^[A-Z]{2}$/i,
+    cep: /^\d{5}-?\d{3}$/,
+    description: /^.{10,}$/
+}
