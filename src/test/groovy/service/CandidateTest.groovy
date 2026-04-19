9ea8419d69c4e32a0be0109099978ba6ac46689d
Davi <davibispo2004@hotmail.com>
2026-04-19 11:22:11 -0300

    fix: changed the acess logic for the mock repository

--- src/test/groovy/service/CandidateTest.groovy
+++ src/test/groovy/service/CandidateTest.groovy
@@ -2,18 +2,20 @@ package service
 
 import com.davi.model.Candidate
 import com.davi.service.CandidateService
+import com.davi.repository.MemoryCandidateRepository
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import static org.junit.jupiter.api.Assertions.*
 
 class CandidateTest {
 
-    CandidateService candidateService = new CandidateService()
+    MemoryCandidateRepository memoryRepo = new MemoryCandidateRepository()
+    CandidateService candidateService = new CandidateService(memoryRepo)
 
     @Test
     @DisplayName("When a candidate should be sucessfully created")
     void candidateCreatedWithSuccessfully() {
-        Candidate candidate= new Candidate("Jonh Doe", "jonhdoe@gmail.com", "Santa Catarina", "12321312", "Apaixonado por tecnologia", "12312311242", 21)
+        Candidate candidate = new Candidate("Jonh", "Doe", "jonhdoe@gmail.com", "12312311242", "Apaixonado por tecnologia", "pass123", "loc-1")
         candidateService.create(candidate)
         assertEquals(1, candidateService.listAll().size())
     }
@@ -21,14 +23,13 @@ class CandidateTest {
     @Test
     @DisplayName("When a candidate should be sucessfully listed")
     void candidateListedWithSuccessfully() {
-        Candidate firstCandidate = new Candidate("Max", "max@gmail.com", "Rio de Janeiro", "123211232", "Amo estudar", "2312232312", 23)
-        Candidate secondCandidate = new Candidate("Jonh", "jonh12@gmail.com", "São Paulo", "126756232", "Adoro física", "45445454534", 19)
+        Candidate firstCandidate = new Candidate("Max", "Verstappen", "max@gmail.com", "2312232312", "Amo estudar", "pass123", "loc-1")
+        Candidate secondCandidate = new Candidate("Jonh", "Doe", "jonh12@gmail.com", "45445454534", "Adoro física", "pass123", "loc-2")
 
         candidateService.create(firstCandidate)
         candidateService.create(secondCandidate)
 
         assertEquals(2, candidateService.listAll().size())
     }
-
 
 }
--- src/test/groovy/service/CompanyTest.groovy
+++ src/test/groovy/service/CompanyTest.groovy
@@ -2,18 +2,20 @@ package service
 
 import com.davi.model.Company
 import com.davi.service.CompanyService
+import com.davi.repository.MemoryCompanyRepository
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import static org.junit.jupiter.api.Assertions.*
 
-
 class CompanyTest {
-    private CompanyService companyService = new CompanyService()
+
+    MemoryCompanyRepository memoryRepo = new MemoryCompanyRepository()
+    CompanyService companyService = new CompanyService(memoryRepo)
 
     @Test
     @DisplayName("When a company should be sucessfully created")
     void companyCreatedWithSuccessfully() {
-        Company company = new Company('ZG Acelera',   'contato@acelera.com.br',   'GO', '01000-000', 'Software house focada em integração e automação.', '12.345.678/0001-90', 'Brazil')
+        Company company = new Company("ZG Acelera", "12.345.678/0001-90", "contato@acelera.com.br", "Software house", "pass123", "loc-1")
         companyService.create(company)
         assertEquals(1, companyService.listAll().size())
     }
@@ -21,15 +23,15 @@ class CompanyTest {
     @Test
     @DisplayName("When a company should be sucessfully listed")
     void companyListedWithSuccessfully() {
-        Company firstCompany = new Company('FlowTech Solutions',   'contato@flowtech.example',   'SP', '01000-000', 'Software house focada em integração e automação.', '12.345.678/0001-90', 'Brazil')
-        Company secondCompany = new Company('Bahia Payments',       'suporte@bahiapay.example',   'BA', '40000-000', 'Serviços de pagamento e adquirência para varejo.',  '98.765.432/0001-10', 'Brazil')
-        Company thirdCompany = new Company('Rio Data Lab',         'hello@riodatalab.example',   'RJ', '20000-000', 'Consultoria em dados, BI e observabilidade.', '45.678.912/0001-33', 'Brazil')
+        Company firstCompany = new Company("FlowTech Solutions", "12345678000190", "contato@flowtech.example", "Software house", "pass123", "loc-1")
+        Company secondCompany = new Company("Bahia Payments", "98765432000110", "suporte@baha", "Serviços", "pass123", "loc-2")
+        Company thirdCompany = new Company("Rio Data Lab", "45678912000133", "hello@rio", "Consultoria", "pass123", "loc-3")
 
         companyService.create(firstCompany)
         companyService.create(secondCompany)
         companyService.create(thirdCompany)
 
-        List<Company> companies = companyService.listAll();
+        List<Map> companies = companyService.listAll()
         assertEquals(3, companyService.listAll().size())
     }
 }
