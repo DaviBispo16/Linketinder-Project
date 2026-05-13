ffca619382532fcd905375dcffd84aac31ffced1
Davi <davibispo2004@hotmail.com>
2026-05-13 00:53:58 -0300

    feat: added a server using tomcat

--- src/main/groovy/com/davi/Main.groovy
+++ src/main/groovy/com/davi/Main.groovy
@@ -1,115 +1,52 @@
 package com.davi
 
+import com.davi.controller.CandidateController
+import com.davi.controller.JobController
+import com.davi.controller.SkillController
 import com.davi.database.ConnectionFactory
 import com.davi.database.DatabaseConnectionFactory
-import com.davi.model.Candidate
-import com.davi.model.Company
 import com.davi.repository.CandidateRepository
 import com.davi.repository.CompanyRepository
 import com.davi.service.CandidateService
 import com.davi.service.CompanyService
+import com.davi.service.JobService
+import com.davi.service.SkillService
 import groovy.sql.Sql
+import org.apache.catalina.Context
+import org.apache.catalina.startup.Tomcat
+
+import java.io.File
 
 static void main(String[] args) {
     ConnectionFactory factory = DatabaseConnectionFactory.getFactory("postgres")
     Sql globalSql = factory.createConnection()
 
-    def companyRepo = new CompanyRepository(globalSql)
     def candidateRepo = new CandidateRepository(globalSql)
 
-    CompanyService companyService = new CompanyService(companyRepo)
     CandidateService candidateService = new CandidateService(candidateRepo)
+    SkillService skillService = new SkillService()
+    JobService jobService = new JobService()
 
-    Scanner scanner = new Scanner(System.in)
-    boolean running = true
-    while (running) {
-        println "1) Listar Empresas"
-        println "2) Listar Candidatos"
-        println "3) Cadastro de Empresa"
-        println "4) Cadastro de Candidato"
-        print "Escolha uma opção: "
+    Tomcat tomcat = new Tomcat()
+    tomcat.setPort(8080)
+    tomcat.getConnector()
 
-        int option = -1
-        if (scanner.hasNextInt()) {
-            option = scanner.nextInt()
-            scanner.nextLine()
-        } else {
-            scanner.nextLine()
-        }
+    Context ctx = tomcat.addContext("", new File(".").getAbsolutePath())
 
-        switch (option) {
-            case 1:
-                companyService.listAll().each { row ->
-                    println """
-                    Nome: ${row.name}
-                    CNPJ: ${row.cnpj}
-                    Email: ${row.email}
-                    Description: ${row.description}
-                    """
-                }
-                break
+    ctx.getServletContext().setAttribute("candidateService", candidateService)
+    ctx.getServletContext().setAttribute("skillService", skillService)
+    ctx.getServletContext().setAttribute("jobService", jobService)
 
-            case 2:
-                candidateService.listAll().each { row ->
-                    println """
-                    Nome: ${row.first_name} ${row.last_name}
-                    Email: ${row.email}
-                    CPF: ${row.cpf}
-                    Description: ${row.description}
-                    """
-                }
-                break
+    Tomcat.addServlet(ctx, "candidateController", new CandidateController())
+    ctx.addServletMappingDecoded("/candidatos", "candidateController")
 
-            case 3:
-                println("Nome:")
-                String name = scanner.nextLine()
-                println("CNPJ:")
-                String cnpj = scanner.nextLine()
-                println("E-mail:")
-                String email = scanner.nextLine()
-                println("Descrição:")
-                String description = scanner.nextLine()
-                println("Senha:")
-                String password = scanner.nextLine()
-                println("Location ID:")
-                String locationId = scanner.nextLine()
+    Tomcat.addServlet(ctx, "skillController", new SkillController())
+    ctx.addServletMappingDecoded("/skills", "skillController")
 
-                Company company = new Company(name, cnpj, email, description, password, locationId)
-                try {
-                    companyService.create(company)
-                    println "Empresa ${company.name} cadastrada com sucesso!"
-                } catch (Exception e) {
-                    println "Erro ao salvar empresa no banco: ${e.message}"
-                }
-                break
+    Tomcat.addServlet(ctx, "jobController", new JobController())
+    ctx.addServletMappingDecoded("/vagas", "jobController")
 
-            case 4:
-                println("Primeiro Nome:")
-                String firstName = scanner.nextLine()
-                println("Sobrenome:")
-                String lastName = scanner.nextLine()
-                println("E-mail:")
-                String email = scanner.nextLine()
-                println("CPF:")
-                String cpf = scanner.nextLine()
-                println("Descrição:")
-                String description = scanner.nextLine()
-                println("Senha:")
-                String password = scanner.nextLine()
-                println("Location ID:")
-                String locationId = scanner.nextLine()
-
-                Candidate candidate = new Candidate(firstName, lastName, email, cpf, description, password, locationId)
-                try {
-                    candidateService.create(candidate)
-                    println "Candidato ${candidate.first_name} ${candidate.last_name} cadastrado com sucesso!"
-                } catch (Exception e) {
-                    println "Erro ao salvar candidato no banco: ${e.message}"
-                }
-                break
-
-            default:
-                println "Opção inválida, tente novamente"
-        }
-    }
+    println "Iniciando servidor Tomcat na porta 8080..."
+    tomcat.start()
+    tomcat.getServer().await()
 }
